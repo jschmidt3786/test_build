6ef8aa2354731e2e882fa497f99c50cbb5ce31a7
Jeff Schmidt <jeff.schmidt@lge.com>
2017-07-13 14:51:03 -0500

    add additional repos to pull script

--- pull_repos.sh
+++ pull_repos.sh
@@ -14,7 +14,7 @@ WORKINGDIR=${HOME}/build
 pushd ${WORKINGDIR} 1> /dev/null
 
 # sync repositories
-REPOLIST="pcdapp linux phoenixphpclient"
+REPOLIST="pcdapp linux phoenixphpclient gbgem gem gemadminclient jdce networkfeeds procentricbootstrapscripts procentriclicensebuilder procentriclicenseutils procentrictlxutils procentricupdatesbuilder triveniepg"
 for repo in ${REPOLIST} ; do
   if [[ -d ${repo} ]] ;then
     echo fetching changes in ${repo} repository
@@ -26,12 +26,25 @@ for repo in ${REPOLIST} ; do
     echo "the ${repo} repository is missing from our working directory"
     echo "possible fixes include:
     cd ${WORKINGDIR}
-    git clone ssh://git@136.166.60.113:7999/pcp/pcdapp.git
-    git clone ssh://git@136.166.60.113:7999/pcp/linux.git
-    git clone git@bitbucket.org:procentric/phoenixphpclient.git"
+    git clone git@136.166.60.113:7999/pcp/pcdapp.git
+    git clone git@136.166.60.113:7999/pcp/linux.git
+    git clone git@bitbucket.org:procentric/phoenixphpclient.git
+    git clone git@bitbucket.org:procentric/gbgem.git
+    git clone git@bitbucket.org:procentric/gem.git
+    git clone git@bitbucket.org:procentric/gemadminclient.git
+    git clone git@bitbucket.org:procentric/jdce.git
+    git clone git@bitbucket.org:procentric/networkfeeds.git
+    git clone git@bitbucket.org:procentric/procentricbootstrapscripts.git
+    git clone git@bitbucket.org:procentric/procentriclicensebuilder.git
+    git clone git@bitbucket.org:procentric/procentriclicenseutils.git
+    git clone git@bitbucket.org:procentric/procentrictlxutils.git
+    git clone git@bitbucket.org:procentric/procentricupdatesbuilder.git
+    git clone git@bitbucket.org:procentric/triveniepg.git
+    "
     exit 1
   fi
 done
 
-pwd
+#env |grep -i agent
+kill ${SSH_AGENT_PID}
 
